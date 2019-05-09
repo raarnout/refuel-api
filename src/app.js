cd1a779cbe3d8f8bc38a26c02e10359f81c44821
Ronald <ronaldaarnoutse@gmail.com>
2019-05-09 21:05:58 +0200

    create node server on port 3000

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('hello nodemon');
+const http = require('http');
+const server = http.createServer((req, res) => {
+	// prints log in terminal when visiting localhost:3000
+	console.log('incomming requests');
+});
+
+server.listen(3000);
