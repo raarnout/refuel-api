4e4b42925cf0ebab1c2121c2f6c2ed6f62fbd2f5
Ronald <ronaldaarnoutse@gmail.com>
2019-05-09 21:33:11 +0200

    add response on different routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+
+	if(url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Enter messages</title></head>');
+		res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+		res.write('</html>');
+		// after res.end() we must not use res.write() or res.header() anymore.
+		// Thats why we return from the anonymous function.
+		return res.end();
+	}
 
 	res.setHeader('Content-Type', 'text/html');
-
 	res.write('<html>');
-	res.write('<head><title>Hello</title></head>');
-	res.write('<body><h1>Hello from localhost:3000</h1></body>');
+	res.write('<body>blabla</body>');
 	res.write('</html>');
+
 
 	res.end();
 });
